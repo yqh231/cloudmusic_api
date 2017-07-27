46a4976d132f4cbed61a0ed433d6704ab9168461
yqh231 <442621753@qq.com>
2017-07-27 08:31:12 +0800

    ##

--- spider/cloud_api/api.py
+++ spider/cloud_api/api.py
@@ -15,13 +15,13 @@ def api_comment(song_id, offset, limit):
     TEXT = {"rid": song_id, "offset": offset, "total": "true", "limit": limit, "csrf_token": ""}
     return generate_secret_key(TEXT), url
 
-def api_search(key_word, offset, limit):
-    url = BASE_URL + 'weapi/search/suggest/web?csrf_token='
-    TEXT = {'s': '天下', 'type': 1, 'offset': 0, 'limit':5}
-    res1 = generate_secret_key(TEXT)
-    import requests
-    import pprint
-    res = requests.post(url, data=res1)
-    print (res.text)
-
-api_search('海阔天空',0,5)
+# def api_search(key_word, offset, limit):
+#     url = BASE_URL + 'weapi/search/suggest/web?csrf_token='
+#     TEXT = {'s': '天下', 'type': 1, 'offset': 0, 'limit':5}
+#     res1 = generate_secret_key(TEXT)
+#     import requests
+#     import pprint
+#     res = requests.post(url, data=res1)
+#     print (res.text)
+#
+# api_search('海阔天空',0,5)
